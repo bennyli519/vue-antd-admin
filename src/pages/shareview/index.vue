ad24aa610893b5c14564d302a8dbc2e9019431bc
BennyLzb <bli@junglescout.com>
2022-09-06 20:52:22 +0800

    feat: modify tab

---
+++ src/pages/shareview/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="height:100vh">
+    ShareView {{$route.query.redirectUrl}}
+    <iframe width="100%" height="100%" :src="$route.query.redirectUrl"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customName',
+  data () {
+    return {
+    }
+  },
+  created(){
+    console.log(this.$route.query)
+  },
+
+  methods: {}
+}
+</script>
